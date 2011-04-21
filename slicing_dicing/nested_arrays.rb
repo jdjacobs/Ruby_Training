9563cc747415081fa76ad5f4673ce5044d62429a
Brian J Reath <brian.reath@ccisystems.com>
2011-04-21 11:14:01 -0500

    Adding more practice scenarios

---
+++ slicing_dicing/nested_arrays.rb
@@ -0,0 +1,13 @@
+values = [
+  [3.2, 7.1, 5.2, 10.6],
+  [20.7, 31.2, 1.1, 4.3],
+  [100.0, 5.0, -30.0, 5.0]
+]
+
+# Print the array in ascending order of the sum of each nested array
+# Print the 2nd element of the array
+# Remove the last element of the array and print
+
+# Flatten the array and store it in a variable
+# Print the new array in ascending order
+# Print the new array in descending order
--- slicing_dicing/subarrays.rb
+++ slicing_dicing/subarrays.rb
@@ -10,6 +10,10 @@ array1 = [1, 2, 10, 3, 2.5, 13]
 
 # Insert the number 7.5 after the 3rd element of array1
 
+# Print the first two elements of array1
+# Print the 2nd through the 5th elements of array1
+# Print the last element of array1
+
 # Print array1 in the reverse order
 # Print array1 in ascending order
 # Print array1 in descending order
